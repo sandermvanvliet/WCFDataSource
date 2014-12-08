78c8f706fc81902b1183f7124a05470ffe8d8c3f
Sander van Vliet <sauron@barad-dur.nl>
2014-12-08 14:52:26 +0100

    Added comment to the schema only reader to clarify what happens

--- SanderVanVliet.WcfDataSource/WcfDataSchemaOnlyReader.cs
+++ SanderVanVliet.WcfDataSource/WcfDataSchemaOnlyReader.cs
@@ -12,6 +12,9 @@ namespace SanderVanVliet.WcfDataSource
 
         protected override object[] GetDataFromOperation()
         {
+            // Return an empty object array to fake a resultset.
+            // We don't even need to create an array of the operation result
+            // because we'll never read from it.
             return new object[0];
         }
     }
